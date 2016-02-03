be3bde9301fbac045467ba0e4d2b95f82b30362e
Kingsley <chukwumakingsley1@gmail.com>
2016-02-03 13:13:09 +0200

    6 authentications and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+
 	validates :name, presence: true, length: {maximum: 50}
 
 	validates :email, presence: true, length: {maximum: 250}
+	validates :password, length: { minimum: 6}
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 250},
 						format: { with: VALID_EMAIL_REGEX },
-						uniqueness: true
+						uniqueness: { case_sensitive: false }
+	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup	
-  	@user = User.new(name: "Example User", email: "user@example.com")
+  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -53,8 +53,14 @@ class UserTest < ActiveSupport::TestCase
 
   test "email address should be unique" do
     duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
 
