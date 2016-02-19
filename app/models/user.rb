578ca49fa8d83bb9444823c5352e7270208f5810
Kingsley <chukwumakingsley1@gmail.com>
2016-02-19 15:14:22 +0200

    v8 cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 	validates :password, presence: true
 	validates :password_confirmation, presence: true
@@ -14,9 +15,20 @@ class User < ActiveRecord::Base
 						uniqueness: { case_sensitive: false }
 	has_secure_password
 
+	#Returns the hash digest of the given string
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 															BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
+
+	#Returns a random token.
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember 
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end 
 end
