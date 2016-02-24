3cc36ca2987f707df1bd69d8b5b26e5ff16505e4
Kingsley <chukwumakingsley1@gmail.com>
2016-02-24 14:50:14 +0200

    v checkbox remember password

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,16 +12,32 @@ module SessionsHelper
   end
 
   #Returns the current logged-in user (if any)
-  def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
+ def current_user
+  if (user_id = session[:user_id])
+    @current_user ||= User.find_by(id: user_id)
+  elsif (user_id = cookies.signed[:user_id])
+    user = User.find_by(id: user_id)
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
+    end
   end
+end
 
   #Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
 
+  #Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
